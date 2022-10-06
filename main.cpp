dafeb5d42e95bbfac408351972714e7d4431cfcd
AnoushkaM1302 <anoushka.m02@gmail.com>
2022-10-06 22:23:43 +0530

    memory functions

--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@ class program
     char R[4];
     int IC, SI;
     char buffer[40];
-    bool C = false;
+    bool C;
 
 public:
     void init();
     void load();
+    int getloc();
     void startExecution();
     void executeUserProgram();
     void MOS();
@@ -32,14 +33,20 @@ void program::init()
             M[i][j] = '\0';
         }
     }
-
+    C = false;
     IR[4] = {' '};
     R[4] = {' '};
 }
 
+int program::getloc()
+{
+    int mem = ((IR[2] - '0') * 10) + (IR[3] - '0');
+    return mem;
+}
+
 void program::load()
 {
-    int m =0;
+    int m = 0;
     do
     {
         for (int i = 0; i < 40; i++)
@@ -79,10 +86,11 @@ void program::load()
 
     int x = 0;
     for (int i = 0; i < 100; i++)
-    {       //printing all memory from 00-99
-            cout << "M" << x << ":" << M[i][0] << M[i][1] << M[i][2] << M[i][3] << endl;
-            x++;
+    { // printing all memory from 00-99
+        cout << "M" << x << ":" << M[i][0] << M[i][1] << M[i][2] << M[i][3] << endl;
+        x++;
     }
+    cout << C;
 }
 
 void program::startExecution()
@@ -93,90 +101,135 @@ void program::startExecution()
 
 void program::executeUserProgram()
 {
-    while(IC<10){
-
-        //reading next instruction
-        IR[0]=M[IC][0];
-        IR[1]=M[IC][1];
-        IR[2]=M[IC][2];
-        IR[3]=M[IC][3];
-
+    int mem;
+    while (IC < 10)
+    {
+        mem = 0;
+        // reading next instruction
+        IR[0] = M[IC][0];
+        IR[1] = M[IC][1];
+        IR[2] = M[IC][2];
+        IR[3] = M[IC][3];
 
         IC++;
 
-
-        if(IR[0]=='P'){
-            SI=2;
+        if (IR[0] == 'P')
+        {
+            SI = 2;
             MOS();
         }
-        else if(IR[0]=='G'){
-            SI=1;
+        else if (IR[0] == 'G')
+        {
+            SI = 1;
             MOS();
         }
-        else if(IR[0]=='L'){
+        else if (IR[0] == 'L')
+        {
+            int loc = getloc();
 
+            for (int i = 0; i < 4; i++)
+            {
+                R[i] = M[loc][i];
+            }
         }
-        else if(IR[0]=='S'){
-            
+        else if (IR[0] == 'S')
+        {
+            int loc = getloc();
+            for (int i = 0; i < 4; i++)
+            {
+                M[loc][i] = R[i];
+            }
         }
-        else if(IR[0]=='B'){
-            
+        else if (IR[0] == 'B')
+        {
+            if (C)
+            {
+                char op_code[2];
+                op_code[0] = IR[2];
+                op_code[1] = IR[3];
+                int new_ic = atoi(op_code);
+                IC = new_ic;
+            }
         }
-        else if(IR[0]=='C'){
-            
+        else if (IR[0] == 'C')
+        {
+            char op_code[2];
+            op_code[0] = IR[2];
+            op_code[1] = IR[3];
+            mem = atoi(op_code);
+
+            if (R[0] == M[mem][0] && R[1] == M[mem][1] && R[2] == M[mem][2] && R[3] == M[mem][3])
+            {
+                C = true;
+            }
+            else
+            {
+                C = false;
+            }
         }
-        else if(IR[0]=='H'){
-            SI=3;
+        else if (IR[0] == 'H')
+        {
+            SI = 3;
             MOS();
         }
     }
 }
 
-void program::MOS(){
-    switch(SI){
-        case 1:
-            read();
-            break;
-        case 2:
-            write();
-            break;
-        case 3:
-            terminate();
-            break;
-        default:
-            break;
+void program::MOS()
+{
+    switch (SI)
+    {
+    case 1:
+        read();
+        break;
+    case 2:
+        write();
+        break;
+    case 3:
+        terminate();
+        break;
+    default:
+        break;
     }
 }
 
-void program::read(){
-  
-    //reading data from data card and putting it into buffer
+void program::read()
+{
+
+    // reading data from data card and putting it into buffer
     Input.getline(buffer, 40);
 
-    //reading OP_CODE from IR and converting it into int data type
+    if (buffer[0] == '$' && buffer[1] == 'E' && buffer[2] == 'N' && buffer[3] == 'D')
+    {
+        return;
+    }
+
+    // reading OP_CODE from IR and converting it into int data type
     char op_code[2];
     op_code[0] = IR[2];
     op_code[1] = IR[3];
 
-    //mem has the op_code
-    int mem = atoi(op_code);    
-    
-    int k = 0;
-            for (int m = mem ; m < mem + 10; m++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    M[m][j] = buffer[k];
-                    k++;
-                }
-            }
+    // mem has the op_code
+    int mem = atoi(op_code);
 
+    int k = 0;
+    for (int m = mem; m < mem + 10; m++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            M[m][j] = buffer[k];
+            k++;
+        }
+    }
+    SI = 0;
 }
 
-void program::write(){
+void program::write()
+{
     cout << "write\n";
 }
-void program::terminate(){
+void program::terminate()
+{
     cout << "terminate\n";
 }
 
